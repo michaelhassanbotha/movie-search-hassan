abfc902269a6e04b44fa0c8f617aa93957bacecd
HASSAN MICHAEL <hassanbotha74@gmail.com>
2025-10-23 17:08:02 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import homeimage from "./home.jpeg"
+
+export default function Home() {
+  return (
+    <div>
+     <h1>Hassan TV, the best TV services</h1>
+     <button className="watch-btn">watch more</button>
+      
+    </div>
+  );
+}
