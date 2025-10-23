47ec9f9f8e389b04a3441815c9927d8983f175f6
HASSAN MICHAEL <hassanbotha74@gmail.com>
2025-10-23 19:04:16 +0200

    feat: add header and navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import homeimage from "./home.jpeg"
-
+const homeimage = "/home.jpeg";
+import "./App.css";
 export default function Home() {
   return (
     <div>
-     <h1>Hassan TV, the best TV services</h1>
-     <button className="watch-btn">watch more</button>
-      
+      <header>
+        <div className="header">
+          <div className="headerList">
+            <a href="#">home</a>
+            <a href="#">about</a>
+          </div>
+
+          <div className="downloads">
+            <a href="#">Downloads</a>
+          </div>
+
+          <div className="log">
+            <a href="#">Login</a>
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
